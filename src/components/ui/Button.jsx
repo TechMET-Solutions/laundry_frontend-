0653461317db1735fee932c0da5bda6bb406ea08
yashlofficial1083-hue <yashlofficial1083@gmail.com>
2026-01-09 14:08:57 +0530

    components

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+const Button = ({
+  btnText,
+  variant = "outline",
+  className = "",
+}) => {
+  const baseStyles =
+    "w-auto min-w-[28] sm:min-w-[32] px-4 py-2 rounded-md font-sans font-semibold text-center outline-none transition";
 
-function Button() {
+  const variants = {
+    outline:
+      "border-2 border-[#1B76BB] bg-white text-[#1B76BB]",
+    primary:
+      "border-2 border-[#1B76BB] bg-[#1B76BB] text-white",
+    danger:
+      "border-2 border-red-600 bg-red-600 text-white",
+    ghost:
+      "border-0 bg-transparent text-[#1B76BB]",
+  };
+
   return (
-    <div>Button</div>
-  )
-}
+    <button className={`${baseStyles} ${variants[variant]} ${className}`}>
+      {btnText}
+    </button>
+  );
+};
 
-export default Button
+export default Button;
---
+++ src/components/ui/DateInputField.jsx
@@ -0,0 +1,21 @@
+
+const DateInputField = ({ labelText }) => {
+  return (
+    <div className="flex flex-col w-full max-w-md font-sans gap-2">
+      <label className="text-sm sm:text-base font-medium leading-6 text-gray-700 flex items-center gap-1">
+        {labelText}  <span className="text-red-600">*</span>
+      </label>
+
+      <div className="relative w-full">
+        <input
+          type="date"
+          className="w-full h-10 rounded-lg border border-gray-400 px-2 appearance-none pr-10 text-sm sm:text-base outline-none focus:border-blue-400 focus:ring-1 focus:ring-blue-300"
+          
+        />
+        
+      </div>
+    </div>
+  );
+};
+
+export default DateInputField;
